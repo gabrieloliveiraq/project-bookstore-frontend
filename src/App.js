e356ef67b113517e37eafd9e1ec687ca36188a2f
Gabriel Domingos de Oliveira <gabrieloliverdev@outlook.com>
2023-09-02 18:47:24 -0300

    shine the header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
 import './App.css';
+import Logo from './components/Logo/index.js';
+import profile from './images/perfil.svg'
+import checkout from './images/sacola.svg'
+
+const optionsHeader = ['CATEGORIAS', 'FAVORITOS', 'MINHA ESTANTE']
+const icons = [profile, checkout]
 
 function App() {
   return (
-    <div >
-      <header>
-      
+    <div className='App'>
+      <header className='App-header'>
+        <Logo></Logo>
+        <ul className='options'>
+          {optionsHeader.map((texto) => (
+            <li className='option'><p>{texto}</p></li>
+          ))}
+        </ul>
+        
+        <ul className='icons'>
+          {icons.map((icon) => (
+            <li className='icon'><img src={icon} alt='icons' /></li>
+          ))}
+        </ul>
       </header>
     </div>
   );
