b35ae89f21d978e11829e834d21f9b0863dc3b08
Gabriel Domingos de Oliveira <gabrieloliverdev@outlook.com>
2023-09-07 16:11:19 -0300

    refactoring the code

--- src/App.js
+++ src/App.js
@@ -1,28 +1,10 @@
 import './App.css';
-import Logo from './components/Logo/index.js';
-import profile from './images/perfil.svg'
-import checkout from './images/sacola.svg'
-
-const optionsHeader = ['CATEGORIAS', 'FAVORITOS', 'MINHA ESTANTE']
-const icons = [profile, checkout]
+import Header from './components/Header';
 
 function App() {
   return (
     <div className='App'>
-      <header className='App-header'>
-        <Logo></Logo>
-        <ul className='options'>
-          {optionsHeader.map((texto) => (
-            <li className='option'><p>{texto}</p></li>
-          ))}
-        </ul>
-        
-        <ul className='icons'>
-          {icons.map((icon) => (
-            <li className='icon'><img src={icon} alt='icons' /></li>
-          ))}
-        </ul>
-      </header>
+      <Header/>
     </div>
   );
 }
